899f2280121414b0f6cb9f10f1a7558b9e6918f1
Joshua Krall <joshuakrall@pobox.com>
2010-06-25 01:57:18 -0500

    Get rid of htmlunit, which was causing problems Using javax xml parsing instead.

--- src/main/java/hudson/plugins/campfire/Campfire.java
+++ src/main/java/hudson/plugins/campfire/Campfire.java
@@ -13,14 +13,20 @@ import javax.xml.xpath.XPathExpressionException;
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
 
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.xml.XmlPage;
-import com.gargoylesoftware.htmlunit.xml.XmlElement;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import java.io.StringReader;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
 public class Campfire {
     private HttpClient client;
@@ -38,10 +44,10 @@ public class Campfire {
         Credentials defaultcreds = new UsernamePasswordCredentials(token, "x");
         client.getState().setCredentials(new AuthScope(getHost(), -1, AuthScope.ANY_REALM), defaultcreds);
         client.getParams().setAuthenticationPreemptive(true);
-        client.getParams().setParameter("http.useragent", "JTinder");
+        client.getParams().setParameter("http.useragent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-us) AppleWebKit/533.16 (KHTML, like Gecko) Version/5.0 Safari/533.16");
         webClient = new WebClient();
         webClient.setWebConnection(new HttpClientBackedWebConnection(webClient, client));
-        webClient.setJavaScriptEnabled(false);
+        webClient.setJavaScriptEnabled(true);
         webClient.setCookiesEnabled(true);
     }
 
@@ -65,8 +71,15 @@ public class Campfire {
         }
     }
 
-    public XmlPage get(String url) throws IOException {
-        return (XmlPage) webClient.getPage(getProtocol() + getHost() + "/" + url);
+    public String get(String url) throws IOException {
+        GetMethod get = new GetMethod(getProtocol() + getHost() + "/" + url);
+        get.setRequestHeader("Content-Type", "application/xml");
+        try {
+            client.executeMethod(get);
+            return get.getResponseBodyAsString();
+        } finally {
+            get.releaseConnection();
+        }
     }
 
     public boolean verify(int returnCode) {
@@ -74,10 +87,27 @@ public class Campfire {
     }
 
     private List<Room> getRooms() throws IOException, ParserConfigurationException, SAXException, XPathExpressionException {
-        XmlPage page = get("rooms.xml");
+        String body = get("rooms.xml");
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        StringReader reader = new StringReader(body);
+        InputSource inputSource = new InputSource( reader );
+        Document doc = builder.parse(inputSource);
+
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        XPathExpression roomExpr = xpath.compile("//room");
+        XPathExpression nameExpr = xpath.compile(".//name");
+        XPathExpression idExpr = xpath.compile(".//id");
+
+        NodeList roomNodeList = (NodeList) roomExpr.evaluate(doc, XPathConstants.NODESET);
         List<Room> rooms = new ArrayList<Room>();
-        for (XmlElement roomElement : (List<XmlElement>) page.getByXPath("//room")) {
-            rooms.add(new Room(this, ((XmlElement) roomElement.getByXPath(".//name").get(0)).getTextContent().trim(), ((XmlElement) roomElement.getByXPath(".//id").get(0)).getTextContent().trim()));
+        for (int i = 0; i < roomNodeList.getLength(); i++) {
+            Node roomNode = roomNodeList.item(i);
+            String name = ((NodeList) nameExpr.evaluate(roomNode, XPathConstants.NODESET)).item(0).getFirstChild().getNodeValue();
+            String id = ((NodeList) idExpr.evaluate(roomNode, XPathConstants.NODESET)).item(0).getFirstChild().getNodeValue();
+            rooms.add(new Room(this, name.trim(), id.trim()));
         }
         return rooms;
     }
