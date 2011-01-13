0c990ff2267c801f7fa1da3f055b8ecd4c44622c
thickpaddy <mwoods@online.ie>
2011-01-13 17:41:50 +0000

    Only include build url in failure notifications

--- src/main/java/hudson/plugins/campfire/CampfireNotifier.java
+++ src/main/java/hudson/plugins/campfire/CampfireNotifier.java
@@ -75,8 +75,8 @@ public class CampfireNotifier extends Notifier {
         String resultString = result.toString();
         if (!DESCRIPTOR.getSmartNotify() && result == Result.SUCCESS) resultString = resultString.toLowerCase();
         String message = build.getProject().getName() + " " + build.getDisplayName() + " \"" + changeString + "\": " + resultString;
-        if (hudsonUrl != null && hudsonUrl.length() > 1) {
-            message = message + "\n" + hudsonUrl + build.getUrl();
+        if (hudsonUrl != null && hudsonUrl.length() > 1 && (DESCRIPTOR.getSmartNotify() || result != Result.SUCCESS)) {
+            message = message + " (" + hudsonUrl + build.getUrl() + ")";
         }
         room.speak(message);
     }
